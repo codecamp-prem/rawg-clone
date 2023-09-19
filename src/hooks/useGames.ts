5824f4cd587397362ebaa7706423872ce4f668eb
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-20 03:45:31 +0545

    show loading skeleto with chakra ui skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,24 +18,36 @@ interface FetchGamesResponse {
   results: Game[];
 }
 const useGames = () => {
+  const [isLoading, setLoading] = useState(false);
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
+    //.finally(() => {
+    // get called when success or error so we don't
+    // have to setLoading in .then and .catch
+    // doesn't work in <React.StrictMode>
+    //setLoading(false);
+    //});
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
